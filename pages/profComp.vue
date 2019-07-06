d599fe03b0cc397c72c209b1e450562ed6fa3162
takaHAL <takaonoyuki0610@gmail.com>
2019-07-06 12:38:43 +0900

    入力完了画面（仮）

--- pages/profComp.vue
+++ pages/profComp.vue
@@ -1,5 +1,24 @@
 <template>
-
+<div>
+<h3 class="text-xs-center my-4">SNS連携をする事で<br />このアプリの機能を全て使えます。</h3>
+        <v-btn color="#1DCD00">
+            Lineを連携させる
+        </v-btn>
+        
+          <v-btn color="#3DCDEF">
+            Twitterを連携させる
+        </v-btn>
+         <v-container >
+         <h3 class="text-xs-center my-4">利用規約</h3>
+             <v-card color="purple" dark 
+             style="width: 80vw;  margin: 0 25px;"
+             >
+              <v-card-text>この利用規約（以下，「本規約」といいます。）は，＿＿＿＿＿（以下，「当社」といいます。）がこのウェブサイト上で提供するサービス（以下，「本サービス」といいます。）の利用条件を定めるものです。登録ユーザーの皆さま（以下，「ユーザー」といいます。）には，本規約に従って，本サービスをご利用いただきます。</v-card-text>
+         </v-card>
+       
+        </v-container>
+ 
+</div>
 </template>
 
 <script>
@@ -8,6 +27,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scope>
+.v-btn {
+   margin-bottom: 32px;
+  }
 </style>
