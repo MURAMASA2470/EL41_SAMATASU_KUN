73f5673b26c12ae2295efff62b8d07f2ffb20ada
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2019-07-06 16:25:46 +0900

    task dark

---
+++ pages/task_dark.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        
+        <div>
+            <h3 class="text-xs-center my-4">宿題一覧</h3>
+
+            <v-sheet class="work-item" v-for="(subject, i) in x['教科']" :key="i" color="black">
+                
+                    <label class="work-content">{{ subject['名前'] }} の {{ x['種類'][i]['名前'] }}</label>
+                    <label class="work-amount">{{5+i}}{{ x['種類'][i]['単位'] }}</label>
+                
+            </v-sheet>
+
+        </div>
+
+        <div class="next-btn">
+            <v-btn color="#EB8712" round to='profComp'>
+                課題を提出する
+            </v-btn>
+        </div>
+
+        
+    </div>
+</template>
+
+<style scoped>
+
+    * {
+        color: red;
+    }
+
+    .v-content__wrap {
+        background-color: black !important;
+    }
+
+    .v-dialog .v-btn {
+        width: 100%;
+        margin: 40px 0;
+    }
+
+        .card-body {
+            padding-top: 30px;
+        }
+
+            .dialog-text {
+                padding: 10px 0;
+                font-size: 1.2rem;
+            }
+
+    .next-btn {
+        position: absolute;
+        bottom: 35px;
+    }    
+
+    .work-item {
+        height: 60px;
+        border-radius: 10px;
+        margin: 25px 30px;
+        font-size: 1.5rem;
+        padding: 5px 10px;
+        position: relative;
+        background-color: black;
+        border: solid 2ps white;
+    }
+
+        .work-content {
+            position: absolute;
+            text-align: left;
+            left: 15px;
+            top: 25%;
+        }
+
+        .work-amount {
+            position: absolute;
+            text-align: right;
+            right: 45px;
+            top: 25%;
+        }
+</style>
+
+<script>
+import setting from "../config/samatasu.json"
+
+console.log(setting['教科'][0]['色']);
+
+export default {
+    data: () => {
+        return {
+            dialog: false,
+            x: setting, 
+            subjectList: setting['教科'].map((x) => {
+                return x['名前'];
+            }), 
+            genreList : setting['種類'].map((x) => {
+                return x['名前'];
+            }), 
+            amountList: [...Array(99).keys()].map(i => ++i)
+            
+        }
+    }, 
+    created: () => {
+     
+    }
+}
+</script>
