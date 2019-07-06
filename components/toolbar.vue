5e757cc33bcf384089015e5ec5d4cf547d9aea7e
takaHAL <takaonoyuki0610@gmail.com>
2019-07-06 10:41:34 +0900

    toolbar追加

---
+++ components/toolbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-toolbar
+      fixed
+      app
+    >
+
+      <v-btn
+        icon
+         v-if="back"
+        :to="backLink"
+      >
+        <v-icon>arrow_back_ios</v-icon>
+      </v-btn>
+       <v-spacer></v-spacer>  
+     <v-btn icon to="/search" v-if="searchIcon" >
+        <v-icon >search</v-icon>
+      </v-btn>
+      
+    </v-toolbar>
+</template>
+<script>
+export default {
+  props:['searchIcon','back','backLink']
+}
+</script>
---
+++ pages/profInput.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <toolbar back=true />
+</div>
+</template>
+<script>
+import toolbar from '~/components/toolbar.vue'
+export default {
+components: {  
+   toolbar
+  }
+}
+</script>
